f593772eaff941370a31f2f5de8086d03f4656eb
adrien-coquillat <adrien.coquillat@laplateforme.io>
2021-03-08 10:55:59 +0100

    jobifn

---
+++ Jour01/Job04/script.js
@@ -0,0 +1,24 @@
+// function bisextile(annee){
+     
+//      var annee = 365;
+//      if (annee = 365){
+//      return false 
+//      }
+//      else
+//      {
+//      return true
+// }
+// }  
+
+
+console.log(bisextile(2020)); 
+function bisextile(annee)
+{
+     
+if ((annee%4==0) && ((annee%100!=0) || (annee%400==0))) return true;
+else return false;
+}
+
+
+
+
---
+++ Jour01/Job05/script.js
@@ -0,0 +1,14 @@
+function afficherJoursSemaines(){
+    var jourssemaines = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+
+    for(var i= 0; i < 7; i++)
+    {
+        //  document.write(jourssemaines[i]);
+        console.log(jourssemaines[i]);
+    }
+}
+afficherJoursSemaines();
+
+
+
+
